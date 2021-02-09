18e31463becfd2d9ddfa5b5f14798c2d13728b94
Eryk-Luiz <erykluizvieira@gmail.com>
2021-02-08 22:26:05 -0300

    Static props on Landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
+import { GetStaticProps } from 'next';
 import React from 'react';
 import LandingPage from '../layout/LandingPage';
+import api from '../services/api';
 
-const Landing: React.FC = () => <LandingPage />;
+type LandingProps = {
+  open_issues_count: number;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/repos/backend-br/vagas');
+
+  return {
+    props: {
+      open_issues_count: data.open_issues_count,
+    },
+    revalidate: 20,
+  };
+};
+const Landing: React.FC<LandingProps> = ({ open_issues_count }) => (
+  <LandingPage open_issues_count={open_issues_count} />
+);
 
 export default Landing;
