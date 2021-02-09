5e0ff70b9c0af34c2122fa3f20e8297d2652bf43
Eryk-Luiz <erykluizvieira@gmail.com>
2021-02-09 20:24:03 -0300

    Added google analitycs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import api from '../services/api';
 import GlobalStyle from '../styles/globalStyle';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const googleId = process.env.gcloud_id;
+
   const [blocked, setBlocked] = useState({
     blocked: false,
     remaining: 0,
@@ -31,6 +33,23 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
+        />
+
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleId}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${googleId}');
+
+        `,
+          }}
         />
       </Head>
       {blocked.blocked ? <BlockedPage /> : <Component {...pageProps} />}
